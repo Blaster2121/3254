ed32550acadc3bee145714f83a2303a65340e3cb
Blaster2121 <167799392+Blaster2121@users.noreply.github.com>
2025-12-13 01:40:14 +0800

    Update ChatPanel.tsx

--- src/components/ChatPanel/ChatPanel.tsx
+++ src/components/ChatPanel/ChatPanel.tsx
@@ -217,7 +217,7 @@ const ChatPanel: React.FC<ChatPanelProps> = ({
           </div>
           {isDeploymentMessageExpanded && (
             <div style={{ marginTop: '8px' }}>
-              The infrastructure and code we developed are fully capable of integrating our POE bot into this webpage, enabling users to interact with it directly in this chat space. This is demonstrated in our presenation video. However, due to budget constraints, we couldn't find suitable deployment tools that can deploy both the frontend and backend of our software. As a result, currently the backend of the software is not deployed and users cannot chat with our POE bot directly on our website. To try our POE legal bot, please visit <a href="https://poe.com/Doccie" target="_blank" rel="noreferrer" style={{ color: '#856404', textDecoration: 'underline' }}>https://poe.com/Doccie</a> directly.
+              The infrastructure and code we developed are fully capable of integrating our POE bot into this webpage, enabling users to interact with it directly in this chat space. This is demonstrated in our presentation video. However, due to budget constraints, we were unable to find suitable deployment tools that could deploy both the frontend and backend of our software. As a result, currently the backend of the software is not deployed and users cannot chat with our POE bot directly on our website. To try our POE legal bot, please visit <a href="https://poe.com/Doccie" target="_blank" rel="noreferrer" style={{ color: '#856404', textDecoration: 'underline' }}>https://poe.com/Doccie</a> directly.
             </div>
           )}
         </div>
